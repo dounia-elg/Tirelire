531d6254fa92fb5a927dba79ced37d3776c436b7
dounia-elg <dounia.elg20@gmail.com>
2025-10-07 16:47:03 +0100

    Prepar db.js and server.js to connect database

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send("Tirlire API is running");
+});
+
+app.listen(PORT, () => {
+    console.log("Server running on http://localhost:${PORT}");
+});
