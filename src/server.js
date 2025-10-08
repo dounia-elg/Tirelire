894093436e32ffaf933d9edaa4e197c5e39076a5
dounia-elg <dounia.elg20@gmail.com>
2025-10-08 12:13:01 +0100

    await connectDB before starting HTTP server

--- src/server.js
+++ src/server.js
@@ -1,22 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
+import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
-const app = express();
-app.use(express.json());
 
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-connectDB();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Tirlire API is running");
-});
+await connectDB();
+
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
-    console.log("Server running on http://localhost:${PORT}");
+  console.log(` Server running on http://localhost:${PORT}`);
 });
-
-app.use("/users", userRoutes);
