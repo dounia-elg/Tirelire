d34fa53a215345a953dcd03863fa66c8301eed27
dounia-elg <dounia.elg20@gmail.com>
2025-10-10 06:30:02 +0100

    Hash password with bcrypt before saving

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 class User {
   constructor() {
@@ -36,6 +37,17 @@ class User {
       },
       { timestamps: true }
     );
+
+    userSchema.pre("save", async function (next) {
+      if (!this.isModified("password")) return next();
+      try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+      } catch (err) {
+        next(err);
+      }
+    });
 
     this.model = mongoose.models.User || mongoose.model("User", userSchema);
   }
