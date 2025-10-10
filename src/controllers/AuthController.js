f7b06d2ae5f9e6ff231642c33f5efc7cabce922e
dounia-elg <dounia.elg20@gmail.com>
2025-10-10 06:44:11 +0100

    Link auth route in server.js

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,4 +1,3 @@
-// src/controllers/AuthController.js
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 connectDB(); 
@@ -10,7 +11,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
 app.use("/api/users", userRoutes);
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
