cbf5d7d7303f64f0b763e39571f71052f27a4c5f
dounia-elg <dounia.elg20@gmail.com>
2025-10-13 14:34:39 +0100

    Add Group model (name, amount, frequency, creator, members[])

---
+++ src/models/Group.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+class Group {
+  constructor() {
+    const groupSchema = new mongoose.Schema(
+      {
+        name: { type: String, required: true, trim: true },
+        amount: { type: Number, required: true, min: [1, "Amount must be > 0"] },
+        frequency: { type: Number, required: true, min: [1, "Frequency must be > 0"] },
+        creator: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+        members: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+      },
+      { timestamps: true }
+    );
+
+    groupSchema.index({ name: 1, creator: 1 }, { unique: false });
+
+    this.model = mongoose.models.Group || mongoose.model("Group", groupSchema);
+  }
+
+  getModel() {
+    return this.model;
+  }
+}
+
+export default new Group().getModel();
+
+
