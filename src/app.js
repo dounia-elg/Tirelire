64f28d1a12207c5ed646e62ed005ae1e74ad9643
dounia-elg <dounia.elg20@gmail.com>
2025-10-17 12:00:50 +0100

    Fix paymentRoutes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import userRoutes from "./routes/userRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import groupRoutes from "./routes/groupRoutes.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
 import kycRoutes from "./routes/kycRoutes.js";
 
 
@@ -23,9 +24,10 @@ app.get("/health", (req, res) => {
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/groups", groupRoutes);
+app.use("/api/contributions", paymentRoutes);
 
 
-app.use("/api/contributions", require("./routes/paymentRoutes.js").default);
+
 
 export default app;
 
